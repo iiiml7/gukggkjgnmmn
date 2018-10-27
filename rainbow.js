2f9afbd93a2718f48d955fd5f06782c897893d70
iiiml7 <44521591+iiiml7@users.noreply.github.com>
2018-10-27 18:03:47 +0300

    Update rainbow.js

--- rainbow.js
+++ rainbow.js
@@ -1,11 +1,36 @@
 const {Client} = require('discord.js');
 const client = new Client();
+const rainbow = new Array(size);
+
+for (var i=0; i<size; i++) {
+  var red   = sin_to_hex(i, 0 * Math.PI * 2/3); // 0   deg
+  var blue  = sin_to_hex(i, 1 * Math.PI * 2/3); // 120 deg
+  var green = sin_to_hex(i, 2 * Math.PI * 2/3); // 240 deg
+
+  rainbow[i] = '#'+ red + green + blue;
+}
 
 client.on('ready', () => {
     setInterval(function(){
-        client.guilds.get('333278908357017612').roles.find('name', 'R').edit({color: '1,2,3,4,5,6,7,8,9,10'}) 
+        client.guilds.get('333278908357017612').roles.find('name', 'R').edit({color: 'rainbow'}) 
     },50);
 
 });
-
+client.on('ready', function(){	
+    var ms = 4000000 ;	
+    var setGame = ['Turbo Army'];	
+    var i = -1;	
+    var j = 0;	
+    setInterval(function (){	
+        if( i == -1 ){	
+j = 1;	
+       }	
+        if( i == (setGame.length)-1 ){	
+            j = -1;	
+      }	
+       i = i+j;	
+        client.user.setGame(setGame[i],`http://www.youtube.com/gg`);	
+}, ms);	
+	
+});
 client.login('NDEwMjg2OTA2MTk5NTA2OTQ0.DrXGxg.FxiDwR3Im2kuNsnaT80gOTeXSG0');
