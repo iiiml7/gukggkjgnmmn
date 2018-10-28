a3fc37a70dbf2a75a12d5f227e3b9674054fcf5c
iiiml7 <44521591+iiiml7@users.noreply.github.com>
2018-10-28 16:50:22 +0300

    Update rainbow.js

--- rainbow.js
+++ rainbow.js
@@ -6,15 +6,5 @@ client.on('ready', () => {
         client.guilds.get('474651676880535554').roles.find('name', 'R').edit({color: 'RANDOM'}) 
     },1500);
 
-});
-client.on('ready', () => {
-    client.user.setStatus('available')
-    client.user.setPresence({
-        game: {
-            name: 'K i R o X | Rainbow',
-            type: "STREAMING",
-            url: "https://www.twitch.tv/iiiML7"
-        }
-    });
-});
+
 client.login('NDEwMjg2OTA2MTk5NTA2OTQ0.DrdCFA.FJYFhzt4KMxuoXy1DVKVt30JuOo');
