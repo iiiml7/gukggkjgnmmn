4b8e811dce6fcdb8f3762545578215134a116909
iiiml7 <44521591+iiiml7@users.noreply.github.com>
2018-10-28 16:53:15 +0300

    Update rainbow.js

--- rainbow.js
+++ rainbow.js
@@ -5,6 +5,7 @@ client.on('ready', () => {
     setInterval(function(){
         client.guilds.get('474651676880535554').roles.find('name', 'R').edit({color: 'RANDOM'}) 
     },1500);
-
+    
+};
 
 client.login('NDEwMjg2OTA2MTk5NTA2OTQ0.DrdCFA.FJYFhzt4KMxuoXy1DVKVt30JuOo');
