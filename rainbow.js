98877d111be44d0b1d642a6d69daf91d4e2204e4
iiiml7 <44521591+iiiml7@users.noreply.github.com>
2018-10-28 15:41:34 +0300

    Update rainbow.js

--- rainbow.js
+++ rainbow.js
@@ -7,21 +7,14 @@ client.on('ready', () => {
     },60000);
 
 });
-client.on('ready', function(){	
-    var ms = 100000 ;	
-    var setGame = ['MLOO7'];	
-    var i = -1;	
-    var j = 0;	
-    setInterval(function (){	
-        if( i == -1 ){	
-j = 1;	
-       }	
-        if( i == (setGame.length)-1 ){	
-            j = -1;	
-      }	
-       i = i+j;	
-        client.user.setGame(setGame[i],`http://www.youtube.com/gg`);	
-}, ms);	
-	
+bot.on('ready', () => {
+    bot.user.setStatus('available')
+    bot.user.setPresence({
+        game: {
+            name: 'with depression',
+            type: "STREAMING",
+            url: "https://www.twitch.tv/monstercat"
+        }
+    });
 });
 client.login('NDEwMjg2OTA2MTk5NTA2OTQ0.DrXGxg.FxiDwR3Im2kuNsnaT80gOTeXSG0');
